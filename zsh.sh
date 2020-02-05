13770349aba8109a154611accf099d105a717298
Mahbub Zulkarnain <prof.bub@live.com>
2020-02-05 12:14:50 +0700

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -5,8 +5,8 @@ sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install
 
 cd ~ || exit
 
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
 wget -q https://raw.githubusercontent.com/mahbubzulkarnain/setup/master/dotfile/zsh/zul.zsh-theme -O ~/.oh-my-zsh/themes/zul.zsh-theme
 wget -q https://raw.githubusercontent.com/mahbubzulkarnain/setup/master/dotfile/zsh/.zshrc -O ~/.zshrc
