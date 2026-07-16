7b373e2df629295a47b69e79e5c4cb5aebfaf8c3
CODE.ID <mahbub.zulkarnain@gmail.com>
2026-07-16 22:29:14 +0700

    Add windows.sh: MSYS2 entry point chaining git.sh and zsh.sh

---
+++ windows.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+bash <(curl -s https://raw.githubusercontent.com/mahbubzulkarnain/setup/master/git.sh)
+bash <(curl -s https://raw.githubusercontent.com/mahbubzulkarnain/setup/master/zsh.sh)
