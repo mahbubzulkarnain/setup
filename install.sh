1d206e8c53e75a676240f90152ad9b0dbc4beef8
Mahbub Zulkarnain <mahbub.zulkarnain@gmail.com>
2026-07-16 22:31:31 +0700

    Route MSYS2 sessions to windows.sh in install.sh, remove dead OS stub

--- install.sh
+++ install.sh
@@ -8,23 +8,8 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     bash <(curl -s https://raw.githubusercontent.com/mahbubzulkarnain/setup/master/mac.sh)
+elif [[ -n "$MSYSTEM" ]]; then
+    bash <(curl -s https://raw.githubusercontent.com/mahbubzulkarnain/setup/master/windows.sh)
 fi
 
 echo "Link start!!!"
-
-#elif [[ "$OSTYPE" == "cygwin" ]]; then
-#        # POSIX compatibility layer and Linux environment emulation for Windows
-#        echo 'mac'
-#elif [[ "$OSTYPE" == "msys" ]]; then
-#        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-#        echo 'mac'
-#elif [[ "$OSTYPE" == "win32" ]]; then
-#        # I'm not sure this can happen.
-#        echo 'mac'
-#elif [[ "$OSTYPE" == "freebsd"* ]]; then
-#        # ...
-#        echo 'mac'
-#else
-#        # Unknown.
-#        echo 'undefined'
-#fi
