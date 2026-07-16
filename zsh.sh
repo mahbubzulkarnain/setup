3b9907e20a62a71c5716e9aa353be40cf8c3d177
Mahbub Zulkarnain <mahbub.zulkarnain@gmail.com>
2026-07-16 22:19:31 +0700

    Fix zsh.sh: install zsh explicitly (oh-my-zsh installer never does), add MSYS2 branch

--- zsh.sh
+++ zsh.sh
@@ -1,22 +1,35 @@
 #!/usr/bin/env bash
 
 if ! command -v zsh &>/dev/null; then
-    echo  "Install ohmyzsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-    if ! command -v brew &>/dev/null; then
-        bash <(curl -s https://raw.githubusercontent.com/mahbubzulkarnain/setup/master/brew.sh)
+    echo "Install zsh..."
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        apt install -y zsh
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        brew install zsh
+    elif [[ -n "$MSYSTEM" ]]; then
+        pacman -S --noconfirm zsh
     fi
 
-    brew install fzf zsh-syntax-highlighting zsh-autosuggestions
-    
+    echo "Install ohmyzsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+    if [[ -n "$MSYSTEM" ]]; then
+        pacman -S --noconfirm fzf
+    else
+        if ! command -v brew &>/dev/null; then
+            bash <(curl -s https://raw.githubusercontent.com/mahbubzulkarnain/setup/master/brew.sh)
+        fi
+
+        brew install fzf zsh-syntax-highlighting zsh-autosuggestions
+    fi
+
     cd ~ || exit
 
     git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
     curl -o ~/.oh-my-zsh/themes/zul.zsh-theme https://raw.githubusercontent.com/mahbubzulkarnain/setup/master/dotfile/zsh/zul.zsh-theme
-    curl -o ~/.zshrc https://raw.githubusercontent.com/mahbubzulkarnain/setup/master/dotfile/zsh/.zshrc 
+    curl -o ~/.zshrc https://raw.githubusercontent.com/mahbubzulkarnain/setup/master/dotfile/zsh/.zshrc
 fi
 
 source ~/.zshrc
