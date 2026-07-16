cb80304dee9715f2c61c233b4a88233f66b7985c
Mahbub Zulkarnain <mahbub.zulkarnain@gmail.com>
2026-07-16 23:51:01 +0700

    Chain npm.sh into windows.sh, matching mac.sh/debian.sh

--- windows.sh
+++ windows.sh
@@ -2,3 +2,4 @@
 
 bash <(curl -s https://raw.githubusercontent.com/mahbubzulkarnain/setup/master/git.sh)
 bash <(curl -s https://raw.githubusercontent.com/mahbubzulkarnain/setup/master/zsh.sh)
+bash <(curl -s https://raw.githubusercontent.com/mahbubzulkarnain/setup/master/npm.sh)
